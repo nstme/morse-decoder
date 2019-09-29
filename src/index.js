7d78bbc014b48ef894c9435b0a9272664cc76d7f
Anastasiia Sierbaieva <51336515+nstme@users.noreply.github.com>
2019-09-28 19:01:14 -0700

    add: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    //convert input string code to array where each array element has 2 symbols
+    let codeArray = expr.match(/.{1,2}/g);
+
+    //create new array with decoded values
+    function decodeDotDash(array) {
+        let newArray = [];
+        for (let entry of array) {
+            if (entry === '00') {
+                newArray.push('$');
+            } else if (entry === '10') {
+                newArray.push('.');
+            } else if (entry === '11') {
+                newArray.push('-');
+            } else {
+                newArray.push('*');
+            }
+        }
+        return newArray;
+    }
+
+    //decode and create new array where each array element has 5 symbols 
+
+    let noDotDashArray = decodeDotDash(codeArray).join('').match(/.{1,5}/g);
+
+    function removeDollar(array) {
+        newArray = [];
+        for (entry of array) {
+            let newEntry = entry.replace(/\$+/, '');
+            newArray.push(newEntry);
+        }
+        return newArray;
+    }
+
+    let noDollarArray = removeDollar(noDotDashArray);
+
+    //decode using matching key-value pairs in MORSE_TABLE
+    MORSE_TABLE['*****'] = ' ';
+    
+    function decodeToString(array) {
+        let decoded = '';
+
+        for (let i = 0; i <= array.length; i++) {
+            for (let key in MORSE_TABLE) {
+                if (array[i] === key) {
+                    decoded = decoded.concat(MORSE_TABLE[key]);
+                }
+            }
+        }
+        return decoded;
+    };
+
+    let decodedString = decodeToString(noDollarArray);
+
+    return decodedString;
+
 }
 
 module.exports = {
